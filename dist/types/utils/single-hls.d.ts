ab2ae2d5ec6f62689e38d5ecf0f0947fee2e813d
srinivas <srinivas625.4u@gmail.com>
2022-05-24 11:21:50 +0530

    Remove duplicated directory

--- dist/types/utils/single-hls.d.ts
+++
@@ -1,8 +0,0 @@
-declare function createHLSVideo(streamDetails: any): {
-  play: () => void;
-  stop: () => void;
-};
-declare const _default: {
-  player: typeof createHLSVideo;
-};
-export default _default;
--- dist/types/utils/utils.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function format(first: string, middle: string, last: string): string;
-export declare const getMediaPath: (name: string) => string;
-export declare const goFullScreen: (elem: any) => void;
-export declare const exitFullScreen: () => void;
-export declare const isFullscreen: () => boolean;
-export declare const sleep: (ms: number) => Promise<unknown>;
